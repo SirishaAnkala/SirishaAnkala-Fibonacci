43da46c5079c5ea3c17944f0290fb9b4a85d223a
SirishaAnkala <sirishaankala123@gmail.com>
2026-01-30 12:23:40 +0530

    done

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,16 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int prev1 = 0;
+        int prev2 = 1;
+        int current = 0;
+        for(int i = 2; i<=n; i++){
+            current = prev1+ prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        return current;
     }
 }
